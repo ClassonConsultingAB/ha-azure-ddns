8ad62c2c58d8db901a943136cb346b2959d0d272
Johan Classon <johan@classon.eu>
2026-08-30 07:30:46 +0000

    Revert to much suppressions

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -61,7 +61,7 @@ function Get-Version {
         return $Version
     }
     Install-GitVersion
-    Exec "dotnet-gitversion $rootPath /output file /outputfile $versionFilePath" | Out-Null
+    Exec "dotnet-gitversion $rootPath /output file /outputfile $versionFilePath"
     $versionInfo = (Get-Content $versionFilePath | ConvertFrom-Json)
     return $versionInfo.LegacySemVerPadded
 }
